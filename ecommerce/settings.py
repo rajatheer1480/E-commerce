a15e0258d6592713b7786d3185b14c2110888d2a
rajat <rajatheer0008@gmail.com>
2022-11-19 18:29:09 +0530

    change database name and password

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shop_db',
-        # 'USER': 'postgres',
-        # 'PASSWORD':'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'postgres',
         
     }
 }
